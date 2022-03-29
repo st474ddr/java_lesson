0f9ccb957d31770b7c43b3c337b2c450adc37840
Hikke <l7365714@gmail.com>
2022-03-29 11:21:43 +0800

    first Jsoup program

--- itcast/xml/jsoup1/src/jsoup1/Jsoup1.java
+++ itcast/xml/jsoup1/src/jsoup1/Jsoup1.java
@@ -2,6 +2,7 @@ package jsoup1;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.File;
@@ -10,10 +11,14 @@ import java.io.IOException;
 public class Jsoup1 {
 
 	public static void main(String[] args) throws IOException {
+		// student.xml path
 		String path = Jsoup1.class.getClassLoader().getResource("student.xml").getPath();
+		// parse
 		Document document = Jsoup.parse(new File(path), "utf-8");
 		Elements elements = document.getElementsByTag("name");
-		System.out.println(elements.size());
+		Element element = elements.get(0);
+		String name = element.text();
+		System.out.println(name);
 	}
 
 }
